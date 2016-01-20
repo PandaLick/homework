a32494cd2cbf2ae7960299d8a6792e63687059ea
Nick Wong <nwinc.co@gmail.com>
2016-01-19 22:43:16 -1000

    Sortable...
    - Added sortable
    - Removed unnecessary methods
    - Disabled email notifications

--- homework.js
+++ homework.js
@@ -7,25 +7,13 @@ if (Meteor.isClient) {
   Template.todo.helpers({
   	tasks: function () {
       return tasks.find({}, { sort: { due: 1 } });
-  	},
-
-    typesOptions: {
-      sortField: 'order',  // defaults to 'order' anyway
-      group: {
-        name: 'typeDefinition',
-        pull: 'clone',
-        put: false
-      },
-      sort: false  // don't allow reordering the types, just the attributes below
-    }
+  	}
   })
 
   Template.task.helpers({
     selected: function () {
-      // this._id refers to ID's of all current tasks
       var allTasks = this._id;
       var selectedTask = Session.get("selected");
-      // scans ID's to find match -> if the ID's match, that one is 'selected'
       if(allTasks == selectedTask){
           return "selected"
       }
@@ -36,7 +24,6 @@ if (Meteor.isClient) {
     'click': function () {
       var taskID = this._id;
       Session.set("selected", taskID);
-      console.log('and 1');
     },
 
     'click .delete': function () {
@@ -47,10 +34,8 @@ if (Meteor.isClient) {
 
   Template.body.events({
     "submit .taskInput": function (event) {
-      // Prevent default browser form submit
       event.preventDefault();
- 
-      // Get value from form element
+
       var name = event.target.name.value;
       var due = event.target.due.value;
       var course = event.target.course.value;
@@ -58,7 +43,7 @@ if (Meteor.isClient) {
       var currentUserId = Meteor.userId();
  
       Meteor.call("addTask", name, due, course, details, currentUserId);
-      Meteor.call("sendEmail", name, due, course, details, currentUserId);
+      // Meteor.call("sendEmail", name, due, course, details, currentUserId);
       
       event.target.name.value = "";
       event.target.due.value = "";
@@ -72,25 +57,9 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
   Meteor.startup(function () {
-    // SyncedCron.add({
-    //   name: 'Daily email notifications.',
-    //   schedule: function(parser) {
-    //     // parser is a later.parse object
-    //     return parser.text('every 10 sec');
-    //   },
-    //   job: function() {
-    //     console.log('cat');
-    //     // Meteor.call("fetchTasks");
-    //   }
-    // });
-
-    console.log('app starting at ' + Date());
-    console.log('yes... i love homework.')
+    console.log('on');
     process.env.MAIL_URL = 'smtp://postmaster@sandboxf116af3c6a2a4f0da3ef34a871b0ee0d.mailgun.org:a42fc913e66edab1d82dc07ceb16a047@smtp.mailgun.org:587';
-
-    Sortable.collections = tasks;
-
-    // SyncedCron.start();
+    // Sortable.collections = tasks;
   });
 
   Meteor.publish("tasks", function (createdBy) {
@@ -104,7 +73,7 @@ Meteor.methods({
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
     }
- 
+
     tasks.insert({
       name: name,
       due: due,
@@ -112,19 +81,15 @@ Meteor.methods({
       details: details,
       createdBy: currentUserId
     });
+
+    console.log('task created');
   },
   sendEmail: function(name, due, course, details, currentUserId) {
-      Email.send({to:Meteor.user().emails[0].address, from:'no-reply@homework.naauao.com', subject:'New Task!', text:'A new task was created! \"' + name + '\" is due on: ' + due + '.  ' + '\n\nHappy working.'});
-      console.log('email sent.');
+      // Email.send({to:Meteor.user().emails[0].address, from:'no-reply@homework.naauao.com', subject:'New Task!', text:'A new task was created! \"' + name + '\" is due on: ' + due + '.  ' + '\n\nHappy working.'});
+      // console.log('email sent.');
   },
   deleteTask: function (taskID) {
     tasks.remove(taskID);
     console.log('deleted task');
-  },
-  printUser: function () {
-    console.log(Meteor.user().emails[0].address);
-  },
-  fetchTasks: function () {
-    return tasks.find({}, {sort: {due: 1, score: -1, name: 1}});
   }
 });
